0bd91a9b7de5b8b75897fce6336aba5b347e73e3
Harumiii._. <77107580+RumiDaNeko@users.noreply.github.com>
2025-10-25 06:31:33 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import vercel from "@astrojs/vercel/serverless";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   output: "server",
-  adapter: vercel()
+  adapter: cloudflare()
 });
